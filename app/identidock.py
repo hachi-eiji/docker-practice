4d070bda1928fb1002ea7fed31d66b45dc88ed10
Eiji Hachiya <hachiyae@gmail.com>
2016-10-10 10:50:01 +0900

    基本的なwebページの作成

--- app/identidock.py
+++ app/identidock.py
@@ -1,7 +1,26 @@
 from flask import Flask
 app = Flask(__name__)
 
+defalut_name = 'Joe Bloggs'
+
 @app.route('/')
+def get_identicion():
+    name = defalut_name
+
+    html = '''
+    <html>
+    <head><title>Identidock</title></head>
+    <body>
+    <form method="POST">
+    Hello <input type="text" name="name" value="{}"/>
+    <input type="submit" value="submit" />
+    </form>
+    <p> you look like a:
+    <img src="/monster/monster.png" />
+    </body>
+    </html>
+    '''.format(name)
+    return html
 
 def hello_world():
     return 'Hello world\n'
