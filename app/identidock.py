1dcc22de3b409a0454848f8bb6ffe92cf04a893d
Eiji Hachiya <hachiyae@gmail.com>
2016-10-10 11:05:10 +0900

    link

--- app/identidock.py
+++ app/identidock.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
+
 app = Flask(__name__)
 
 defalut_name = 'Joe Bloggs'
 
 @app.route('/')
-def get_identicion():
+def mainpage():
     name = defalut_name
 
     html = '''<html>
@@ -20,6 +22,12 @@ def get_identicion():
     </html>
     '''.format(name)
     return html
+
+@app.route('/monster/<name>')
+def get_identicon(name):
+    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+    image = r.content
+    return Response(image, mimetype='image/png')
 
 def hello_world():
     return 'Hello world\n'
